3e4fe41b5028c5932523a3310c1c96a1958885ad
Harsh Patel <harsh3608@gmail.com>
2023-06-13 12:16:46 +0530

    added account controller and required changes

---
+++ LibraryMvc.Core/DTO/AuthenticationResponse.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace LibraryMvc.Core.DTO
+{
+    public class AuthenticationResponse
+    {
+        public string? PersonName { get; set; } = string.Empty;
+        public string? Email { get; set; } = string.Empty;
+        public string? Token { get; set; } = string.Empty;
+        public DateTime? Expiration { get; set; }
+    }
+}
--- LibraryMvc.Core/IdentityEntities/ApplicationUser.cs
+++ LibraryMvc.Core/IdentityEntities/ApplicationUser.cs
@@ -11,5 +11,6 @@ namespace LibraryMvc.Core.IdentityEntities
     {
         public string? PersonName { get; set; }
         public string? Gender { get; set; }
+        public string? UserType { get; set; }
     }
 }
