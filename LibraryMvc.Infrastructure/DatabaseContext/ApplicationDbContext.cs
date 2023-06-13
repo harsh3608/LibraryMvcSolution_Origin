62a8fb26f953e41e106c32b5773115f20f2f7bcb
Harsh Patel <harsh3608@gmail.com>
2023-06-13 11:57:05 +0530

    added database connection , database, identity entities, dbcontext and stylesheet.css

---
+++ LibraryMvc.Infrastructure/DatabaseContext/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+
+using LibraryMvc.Core.IdentityEntities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryMvc.Infrastructure.DatabaseContext
+{
+    public class ApplicationDbContext: IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public ApplicationDbContext()
+        {
+        }
+    }
+}
--- LibraryMvc.UI/Program.cs
+++ LibraryMvc.UI/Program.cs
@@ -1,8 +1,43 @@
+using LibraryMvc.Core.IdentityEntities;
+using LibraryMvc.Infrastructure.DatabaseContext;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+
+//Configuring database connection string
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
+
+
+//Identity
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+{
+    options.Password.RequiredLength = 5;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireDigit = true;
+
+}).AddEntityFrameworkStores<ApplicationDbContext>()
+  .AddDefaultTokenProviders()
+  .AddUserStore<UserStore<ApplicationUser, ApplicationRole, ApplicationDbContext, Guid>>()
+  .AddRoleStore<RoleStore<ApplicationRole, ApplicationDbContext, Guid>>();
+
+//-----------------------------------Build App---------------------------------------------------------
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
