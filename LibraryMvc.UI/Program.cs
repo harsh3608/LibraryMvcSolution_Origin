9e49a2e29b2158b1a8ce33657be8d4c16235cfc0
Harsh Patel <harsh3608@gmail.com>
2023-06-13 15:37:31 +0530

    initialized the ui project

--- LibraryMvc.UI/Program.cs
+++ LibraryMvc.UI/Program.cs
@@ -1,7 +1,41 @@
+using LibraryMvc.Core.IdentityEntities;
+using LibraryMvc.Infrastructure.DatabaseContext;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+//Configuring database connection string
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+
+builder.Services.AddControllersWithViews();
+
+//Identity
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+{
+    options.Password.RequiredLength = 5;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireDigit = true;
+
+}).AddEntityFrameworkStores<ApplicationDbContext>()
+  .AddDefaultTokenProviders()
+  .AddUserStore<UserStore<ApplicationUser, ApplicationRole, ApplicationDbContext, Guid>>()
+  .AddRoleStore<RoleStore<ApplicationRole, ApplicationDbContext, Guid>>();
+
+
+//-----------------------------------Build App---------------------
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
 
 app.Run();
